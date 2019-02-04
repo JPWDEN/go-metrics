c3bef1cb1bf6b10ad68ca431751feb832c0d0a18
Josh Walker <josh.walker@returnpath.com>
2019-02-04 11:27:00 -0700

    Cleanup

--- cpp/primet.cpp
+++ cpp/primet.cpp
@@ -53,7 +53,6 @@ int main()
     //thread t3(DoPrimes, 2 * step, 3 * step - 1);
     //thread t4(DoPrimes, 3 * step, 4 * step - 1);
     //thread t5(DoPrimes, 4 * step, MAX - 1);
-    
     auto t1s = std::chrono::system_clock::now();
     thread t1(DoPrimes, 2, 350000);
     auto t2s = std::chrono::system_clock::now();
@@ -63,28 +62,15 @@ int main()
     auto t4s = std::chrono::system_clock::now();
     thread t4(DoPrimes, 780001, 920000);
     auto t5s = std::chrono::system_clock::now();
-    thread t5(DoPrimes, 920001, MAX);
-    
-    auto t1e = std::chrono::system_clock::now();
+    thread t5(DoPrimes, 920001, MAX);    
     t1.join();
-    auto t2e = std::chrono::system_clock::now();
     t2.join();
-    auto t3e = std::chrono::system_clock::now();
     t3.join();
-    auto t4e = std::chrono::system_clock::now();
     t4.join();
-    auto t5e = std::chrono::system_clock::now();
     t5.join();
     auto end = std::chrono::system_clock::now();
     chrono::duration<double> elapsed = end - start;
-    chrono::duration<double> t1f = t1e - t1s;
-    chrono::duration<double> t2f = t2e - t2s;
-    chrono::duration<double> t3f = t3e - t3s;
-    chrono::duration<double> t4f = t4e - t4s;
-    chrono::duration<double> t5f = t5e - t5s;
 	cout << numPrimes << " prime numbers under " << MAX << endl;
 	cout << "Elapsed time in seconds: " << elapsed.count() << endl;
-    cout << "t1 - t5: " << t1f.count() << ", " << t2f.count() << ", " << t3f.count()
-         << ", " << t4f.count() << ", " << t5f.count() << endl;
     return 0;
 }
